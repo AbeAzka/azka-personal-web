b0898d5ab96e4496d85977e1bb3c03aadb926125
Abe Azka Official YT <96120510+AbeAzka@users.noreply.github.com>
2025-10-02 13:43:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-import Swal from 'sweetalert2';
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function(){
   //alert('Halo! Terima kasih sudah berkunjung!');
-
   Swal.fire({
     title: 'Hello!',
     text: 'This is a basic SweetAlert2 message.',
