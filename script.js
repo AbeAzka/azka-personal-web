53b5e716ab38c8f1fc7d8c3aac29ed1cb54c48b2
Abe Azka Official YT <96120510+AbeAzka@users.noreply.github.com>
2025-10-02 13:45:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function(){
   //alert('Halo! Terima kasih sudah berkunjung!');
   Swal.fire({
-    title: 'Hello!',
-    text: 'This is a basic SweetAlert2 message.',
+    title: 'Halo!',
+    text: 'Terima kasih sudah berkunjung!',
     icon: 'success', // 'success', 'error', 'warning', 'info', 'question'
     confirmButtonText: 'OK'
   });
